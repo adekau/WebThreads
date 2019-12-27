cc1b5dee5874b033a389982e726897f43c41bc25
Alex Dekau <alex.potatofarms@gmail.com>
2019-12-27 17:41:40 -0500

    remove old thread impl

--- src/Thread.ts
+++ src/Thread.ts
@@ -1,19 +1,80 @@
-export type Thread = {
-    readonly worker: Worker;
-    state: 'running' | 'idle';
-    threadOpts: ThreadRunOptions | undefined;
-    _threadReceiveMsg: (e: MessageEvent) => void;
-    run<T extends (...args: any[]) => any>(
-        this: Thread,
-        func: T,
-        ...args: Parameters<T>
-    ): Promise<ReturnType<T>>;
+import { fnToURL } from './helpers';
+import { Message } from './Message';
+import { Task } from './Task';
+
+export type ThreadConfig = {
+    id: number;
+    onTaskDone: (thread: Thread) => void;
+    onTerminate: (thread: Thread) => void;
 };
 
-export type ThreadRunOptions = {
-    resolve: (value: unknown) => void;
-    reject: (reason: unknown) => void;
-};
+export class Thread implements ThreadConfig {
+    private _worker: Worker;
+    public id: number;
+    public onTaskDone: (thread: this) => void;
+    public onTerminate: (thread: this) => void;
+    public state: 'idle' | 'running';
+    public tasks: Task<any>[];
+
+    constructor(config: ThreadConfig) {
+        Object.assign(this, config);
+        this.state = 'idle';
+        this.tasks = [];
+        this._worker = new Worker(fnToURL(workerMain));
+        this._worker.addEventListener('message', this._onMessage.bind(this));
+    }
+
+    private _onMessage(event: MessageEvent): void {
+        const { data } = event;
+        this._handleWorkerMessage(data);
+    }
+
+    private _handleWorkerMessage(message: Message): void {
+        let taskIdx;
+
+        this.tasks.some((task, idx) => {
+            if (message.id === task.id) {
+                taskIdx = idx;
+                return true;
+            } else
+                return false;
+        });
+
+        if (taskIdx === undefined)
+            return;
+
+        const task = this.tasks[taskIdx];
+        if (message.type === 'error')
+            task.reject(new Error(String(message.error)));
+        if (message.type === 'result')
+            task.resolve(message.result);
+
+        this.onTaskDone(this);
+        if (this.tasks.length === 1)
+            this.state = 'idle';
+        this.tasks.splice(taskIdx, 1);
+    }
+
+    public run<T extends (...args: any[]) => any>(task: Task<T>, ...args: Parameters<T>): void {
+        this.tasks.push(task);
+
+        const message: Message = {
+            type: 'run',
+            id: task.id,
+            func: task.func.toString()
+        };
+
+        // add arguments for spawned thread to parse.
+        Object.keys(args).forEach((_, i) => message['argument' + i] = args[i]);
+
+        this.state = 'running';
+        this.postMessage(message, []);
+    };
+
+    public postMessage(msg: { type: 'run' } & Message, transferables: Transferable[]): void {
+        this._worker.postMessage(msg, transferables);
+    }
+}
 
 const workerMain = `function() {
     onmessage = function (ev) {
@@ -21,67 +82,19 @@ const workerMain = `function() {
 
         const args = Object.keys(msg)
             .filter(key => key.match(/^argument/))
-            .sort((a, b) => parseInt(a.slice(8), 10) - parseInt(b.slice(8), 10))
+            .sort((a, b) => parseInt(a.slice(8), 10) - parseInt(b.slice(8), 10)) // remove word 'argument'
             .map(key => msg[key]);
 
         try {
             const result = eval('(' + msg.func + ')').apply(undefined, args);
-            self.postMessage(result);
+            if (result && result.then && result.catch) {
+                result
+                    .then(res => self.postMessage({ type: 'result', id: msg.id, result: res }))
+                    .catch(err => self.postMessage({ type: 'error', id: msg.id, error: err.stack }));
+            } else
+                self.postMessage({ type: 'result', id: msg.id, result });
         } catch (e) {
-            self.postMessage(e.stack);
+            self.postMessage({ type: 'error', id: msg.id, error: e.stack });
         }
     }
-};`
-
-export const Thread = (): Thread => {
-    const worker = new Worker(fnToURL(workerMain));
-
-    const thread: Thread = {
-        state: 'idle',
-
-        worker,
-
-        threadOpts: undefined,
-
-        _threadReceiveMsg: function (this: Thread, e: MessageEvent) {
-            const msg = e.data;
-            this.state = 'idle';
-            if (msg)
-                this.threadOpts?.resolve(msg);
-            else
-                this.threadOpts?.reject('No data returned');
-        },
-
-        run: function (func, ...args): Promise<ReturnType<typeof func>> {
-            if (this.state === 'running')
-                throw Error('Thread is already in a running state.'); // todo: queue up stuff?
-
-            const obj: any = { func: func.toString() };
-            args.forEach((arg, i) => obj['argument' + i] = arg);
-            return new Promise((resolve, reject) => {
-                this.threadOpts = { resolve, reject };
-                this.state = 'running'
-                this.worker.postMessage(obj);
-            });
-        }
-    };
-
-    worker.addEventListener('message', thread._threadReceiveMsg.bind(thread));
-    return thread;
-};
-
-const fnToURL = (func: Function | string) => {
-    const strFn: string = func.toString();
-    const fnBody: string = strFn
-        .substring(
-            strFn.indexOf('{') + 1,
-            strFn.lastIndexOf('}')
-        );
-
-    const blob = new Blob(
-        [fnBody], {
-            type: 'text/javascript'
-        });
-
-    return URL.createObjectURL(blob);
-}
+};`;
--- test/Thread.spec.ts
+++ test/Thread.spec.ts
@@ -1,26 +1,126 @@
-import { Thread } from '../src/Thread';
+import { Thread } from "../src/Thread";
+import { Task } from "../src/Task";
 
-const mult2 = (x: number) => x * 2;
+function threadTerminate(th: Thread) {
+    console.error(th.id + ' terminated');
+}
 
-describe('Threads', () => {
-  it('creates a thread and posts a message on run', () => {
-    const thread = Thread();
-    thread.worker.postMessage = jasmine.createSpy('onmessage');
+describe('Thread', () => {
+    it('creates a thread', () => {
+        const thread = new Thread({
+            id: 1,
+            onTaskDone: jasmine.createSpy('taskDone'),
+            onTerminate: threadTerminate
+        });
 
-    thread.run(mult2, 15);
-
-    expect(thread.worker).toBeDefined();
-    expect(thread.worker.postMessage).toHaveBeenCalledWith({
-      func: mult2.toString(),
-      argument0: 15
+        expect(thread).toBeDefined();
+        expect(thread.id).toBe(1);
+        expect(thread.state).toBe('idle');
+        expect(thread.tasks).toEqual([]);
     });
-  });
 
-  it('runs a function in a worker', async () => {
-    const thread = Thread();
-    const p = await thread.run(mult2, 6);
-    expect(thread.state).toBe('idle');
-    expect(p).toBe(12);
-    expect(await thread.run(mult2, 14)).toBe(28);
-  });
+    it('runs a task on a thread', async () => {
+        const thread = new Thread({
+            id: 1,
+            onTaskDone: jasmine.createSpy('taskDone'),
+            onTerminate: threadTerminate
+        });
+
+        const task = new Task({
+            id: 1,
+            func: (x: number) => x * 2
+        });
+
+        thread.run(task, 15);
+        const result = await task.done();
+        expect(thread.onTaskDone).toHaveBeenCalled();
+        expect(task.state).toBe('done');
+        expect(result).toBe(30);
+    });
+
+    it('runs an async task on a tread', async () => {
+        const thread = new Thread({
+            id: 1,
+            onTaskDone: jasmine.createSpy('taskDone'),
+            onTerminate: threadTerminate
+        });
+
+        const task = new Task({
+            id: 1,
+            // async functions like so will need to be strings or else tsc transorms it
+            // to `__awaiter` which doesn't exist on the worker thread.
+            func: `async x => x * 2`
+        });
+
+        thread.run(task, 17);
+        const result = await task.done();
+        expect(thread.onTaskDone).toHaveBeenCalled();
+        expect(task.state).toBe('done');
+        expect(result).toBe(34);
+    });
+
+    it('adds and removes to the task queue', async () => {
+        const thread = new Thread({
+            id: 1,
+            onTaskDone: jasmine.createSpy('taskDone'),
+            onTerminate: threadTerminate
+        });
+
+        const task = new Task({
+            id: 1,
+            func: () => new Promise((resolve, _) => setTimeout(() => resolve(5), 500))
+        });
+
+        thread.run(task);
+        expect(thread.state).toBe('running');
+        expect(thread.tasks.length).toBe(1);
+        await task.done();
+        expect(thread.state).toBe('idle');
+        expect(thread.tasks.length).toBe(0);
+    });
+
+    it('Queues multiple tasks', async () => {
+        const thread = new Thread({
+            id: 1,
+            onTaskDone: jasmine.createSpy('taskDone'),
+            onTerminate: threadTerminate
+        });
+
+        const task = new Task({
+            id: 1,
+            func: () => new Promise((resolve, _) => setTimeout(() => resolve(5), 500))
+        });
+        const task2 = new Task({
+            id: 2,
+            func: () => new Promise((resolve, _) => setTimeout(() => resolve(5), 750))
+        });
+
+        thread.run(task);
+        thread.run(task2);
+        expect(thread.state).toBe('running');
+        expect(thread.tasks.length).toBe(2);
+        await task.done();
+        expect(thread.state).toBe('running');
+        expect(thread.tasks.length).toBe(1);
+        await task2.done();
+        expect(thread.state).toBe('idle');
+        expect(thread.tasks.length).toBe(0);
+    });
+
+    it('handles rejections', async (done) => {
+        const thread = new Thread({
+            id: 1,
+            onTaskDone: jasmine.createSpy('taskDone'),
+            onTerminate: threadTerminate
+        });
+
+        const task = new Task({
+            id: 1,
+            func: (x: number) => new Promise((res, rej) => x === 0 ? rej('cant divide by 0') : res(5 / x))
+        });
+        thread.run(task, 0);
+        task.done().then(r => {
+            fail(`Should not have resolved (resolved with: ${r})`);
+        }).catch(_ => done());
+    });
 });
