740f53e67a25ec8e31001f8b9587f9f040f4c2f5
Alex Dekau <alex.potatofarms@gmail.com>
2019-12-27 18:05:21 -0500

    extra line in message & reformat

--- src/Message.ts
+++ src/Message.ts
@@ -1,18 +1,19 @@
 export type Message =
-
-    (| {
-        type: 'result';
-        result: unknown;
-    }
-    | {
-        type: 'error';
-        error: unknown;
-    }
-    | {
-        type: 'run';
-        func: string;
-        [k: string]: any;
-    })
+    (
+        | {
+            type: 'result';
+            result: unknown;
+        }
+        | {
+            type: 'error';
+            error: unknown;
+        }
+        | {
+            type: 'run';
+            func: string;
+            [k: string]: any;
+        }
+    )
     & {
         id: number;
     };
