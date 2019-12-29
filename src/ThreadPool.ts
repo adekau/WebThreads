094a83161eb163611fb7793346102fac0e7207df
Alex Dekau <alex.potatofarms@gmail.com>
2019-12-28 19:20:01 -0500

    WIP: ThreadPool

---
+++ src/ThreadPool.ts
@@ -0,0 +1,11 @@
+/**
+ * Manages multiple threads and executes a task on the next available thread.
+ */
+export class ThreadPool {
+    public static poolCount: number = 0;
+    public static taskCount: number = 0;
+
+    constructor() {
+        // WIP
+    }
+}
